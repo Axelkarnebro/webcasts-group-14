f804c282052c158a7af0a9ae42a0c98e0636dc29
Axelkarnebro <axekrn-7@student.ltu.se>
2022-11-25 13:54:05 +0100

    week 4 lab session progress

--- course/urls.py
+++ course/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from .views import index, course_detail
+from .views import index, course_detail, CreateCourse#, class_detail
 
 app_name = 'course'
 
 urlpatterns = [
     path('', index, name='course_list'),
+    # path('class/<slug:slug>', class_detail, name='class_detail'),
+
+    # Course
+    path('', index, name='course_list'),
+    path('create/', CreateCourse.as_view(), name='create_course'),
     path('<slug:slug>', course_detail, name='course_detail'),
 ]
--- course/views.py
+++ course/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 from .models import Course
+from django.views.generic import CreateView
+from django.utils.text import slugify
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
@@ -11,6 +14,19 @@ def index(request):
     }
 
     return render(request, 'pages/course_list.html', context)
+
+class CreateCourse(CreateView):
+    model = Course
+    template_name = 'pages/course_create.html'
+    fields = ['title','price','short_desc','desc','status']
+
+    def form_valid(self, form):
+        form.instance.slug = slugify(form.instance.title)
+        response = super(CreateCourse, self).form_valid(form)
+        return response
+
+    def get_success_url(self):
+        return reverse('course:course_list')
 
 def course_detail(request, slug):
     course = Course.objects.get(slug__exact=slug)
